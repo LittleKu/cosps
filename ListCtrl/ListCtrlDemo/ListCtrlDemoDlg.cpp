7d8493bc4c39c1b9ad832408ac403e6b2b6b6b1b
cconger77 <cconger77@2afb5f34-2eaf-11df-9ac8-ff20597b2c9b>
2011-05-12 08:36:13 +0000

    ListCtrlDemo: small improvement

--- ListCtrl/ListCtrlDemo/ListCtrlDemoDlg.cpp
+++ ListCtrl/ListCtrlDemo/ListCtrlDemoDlg.cpp
@@ -46,52 +46,6 @@ static SrcDirColumnInfo srcDirColumns[] =
     { _T("Directory"),		CL_NONE_CHECK_BOX,    400  }
 };
 
-
-/////////////////////////////////////////////////////////////////////////////
-// CAboutDlg dialog used for App About
-
-class CAboutDlg : public CDialog
-{
-public:
-	CAboutDlg();
-
-// Dialog Data
-	//{{AFX_DATA(CAboutDlg)
-	enum { IDD = IDD_ABOUTBOX };
-	//}}AFX_DATA
-
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CAboutDlg)
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	//}}AFX_VIRTUAL
-
-// Implementation
-protected:
-	//{{AFX_MSG(CAboutDlg)
-	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
-};
-
-CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
-{
-	//{{AFX_DATA_INIT(CAboutDlg)
-	//}}AFX_DATA_INIT
-}
-
-void CAboutDlg::DoDataExchange(CDataExchange* pDX)
-{
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CAboutDlg)
-	//}}AFX_DATA_MAP
-}
-
-BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
-	//{{AFX_MSG_MAP(CAboutDlg)
-		// No message handlers
-	//}}AFX_MSG_MAP
-END_MESSAGE_MAP()
-
 /////////////////////////////////////////////////////////////////////////////
 // CListCtrlDemoDlg dialog
 
@@ -234,8 +188,8 @@ void CListCtrlDemoDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
 	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
 	{
-		CAboutDlg dlgAbout;
-		dlgAbout.DoModal();
+// 		CAboutDlg dlgAbout;
+// 		dlgAbout.DoModal();
 	}
 	else
 	{
--- ListCtrl/ListCtrlDemo/MainDlg.cpp
+++ ListCtrl/ListCtrlDemo/MainDlg.cpp
@@ -12,6 +12,51 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 /////////////////////////////////////////////////////////////////////////////
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+	
+	// Dialog Data
+	//{{AFX_DATA(CAboutDlg)
+	enum { IDD = IDD_ABOUTBOX };
+	//}}AFX_DATA
+	
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAboutDlg)
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+	
+	// Implementation
+protected:
+	//{{AFX_MSG(CAboutDlg)
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+	//{{AFX_DATA_INIT(CAboutDlg)
+	//}}AFX_DATA_INIT
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAboutDlg)
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+//{{AFX_MSG_MAP(CAboutDlg)
+// No message handlers
+//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
 // CMainDlg dialog
 
 
@@ -179,7 +224,14 @@ BOOL CMainDlg::OnCommand(WPARAM wParam, LPARAM lParam)
 		break;
 	case IDM_FILE_EXIT:
 		{
-			AfxTrace("TODO...\n");
+			ASSERT(AfxGetMainWnd());
+			AfxGetMainWnd()->SendMessage(WM_CLOSE);
+		}
+		break;
+	case IDM_HELP_ABOUT:
+		{
+			CAboutDlg dlg;
+			dlg.DoModal();
 		}
 		break;
 	}
