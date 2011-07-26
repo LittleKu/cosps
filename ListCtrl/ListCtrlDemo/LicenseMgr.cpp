4aac496fd2cb66f2648d514992ddf8ebafc8536b
cconger77 <cconger77@2afb5f34-2eaf-11df-9ac8-ff20597b2c9b>
2011-07-26 15:00:40 +0000

    Update LicenseMgr

--- ListCtrl/ListCtrlDemo/LicenseMgr.cpp
+++ ListCtrl/ListCtrlDemo/LicenseMgr.cpp
@@ -34,18 +34,141 @@ CLicenseMgr::~CLicenseMgr()
 
 BOOL CLicenseMgr::IsRegistered()
 {
-	return FALSE;
+	LONG lResult = ERROR_SUCCESS;
+	CString szName, szCode;
+	lResult = ReadRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Name", szName);
+	if(lResult != ERROR_SUCCESS)
+	{
+		return FALSE;
+	}
+	lResult = ReadRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Code", szCode);
+	if(lResult != ERROR_SUCCESS)
+	{
+		return FALSE;
+	}
+
+	BOOL bResult = CRegKeyMgr::CheckKey(szName, szCode);
+	if(!bResult)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
 }
 
 BOOL CLicenseMgr::Register(const CString& szName, const CString& szCode)
 {
 	BOOL bResult = CRegKeyMgr::CheckKey(szName, szCode);
+	if(!bResult)
+	{
+		return FALSE;
+	}
+
+	LONG lResult;
+	lResult = WriteRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Name", szName);
+	lResult = WriteRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Code", szCode);
+
 	return bResult;
 }
 
 BOOL CLicenseMgr::GetLicenseInfo(CString& szName, CString& szCode)
 {
-	szName.Format("%s", "TestUser1");
-	szCode.Format("123456-7890AB-CDEFGH-IJKLMN-OPQRST-UVWXYZ");
+	LONG lResult;
+	lResult = ReadRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Name", szName);
+	if(lResult != ERROR_SUCCESS)
+	{
+		return FALSE;
+	}
+	lResult = ReadRegString(HKEY_LOCAL_MACHINE, "Software\\Code Project\\LicenseInfo", "Registration Code", szCode);
+	if(lResult != ERROR_SUCCESS)
+	{
+		return FALSE;
+	}
 	return TRUE;
+}
+
+LONG CLicenseMgr::WriteRegString(HKEY hKey, LPCTSTR lpSubKey, LPCTSTR lpValueName, LPCTSTR pszData)
+{
+	LONG lResult = ERROR_SUCCESS;
+    HKEY hKeyResult = NULL;
+	do 
+	{
+		// When writing, create the Key if it does not exist
+		lResult = RegCreateKeyEx( hKey, lpSubKey, 0, NULL, REG_OPTION_NON_VOLATILE, 
+			KEY_WRITE, NULL, &hKeyResult, NULL );
+		if(lResult != ERROR_SUCCESS)
+		{
+			break;
+		}
+
+		// Paydirt
+		lResult = RegSetValueEx( hKeyResult, lpValueName, 0, REG_SZ, 
+			reinterpret_cast<const BYTE*>( pszData ),  ( ::lstrlen( pszData ) + 1 ) * sizeof( TCHAR ) );
+	} while (FALSE);
+	
+	// Cleanup...
+    if( NULL != hKeyResult ) 
+	{ 
+		RegCloseKey( hKeyResult ); 
+	}
+    return lResult;
+}
+
+LONG CLicenseMgr::ReadRegString(HKEY hKey, LPCTSTR lpSubKey, LPCTSTR lpValueName, CString &szValue)
+{
+    LONG lResult = ERROR_SUCCESS;
+    HKEY hKeyResult = NULL;
+    DWORD dwSize = 0;
+    BYTE* pcbData = NULL;
+
+	do 
+	{
+		// Open the key - do not create
+		lResult = RegOpenKeyEx( hKey, lpSubKey, 0, KEY_READ, &hKeyResult );
+		// Sanity Check
+		if( ERROR_SUCCESS != lResult ) 
+		{ 
+			break; 
+		}
+
+		// Query for needed buffer size
+		lResult = RegQueryValueEx( hKeyResult, lpValueName, 0, NULL, NULL, &dwSize );
+		// Sanity Check
+		if( ERROR_SUCCESS != lResult ) 
+		{ 
+			break; 
+		}
+
+		// Add '+ sizeof(TCHAR)' in case the string is not
+		//   stored in the registry with a trailing '\0'
+		pcbData = new BYTE[ dwSize + sizeof(TCHAR) ];
+		// Sanity Check
+		if( NULL == pcbData ) 
+		{        
+			lResult = ERROR_NOT_ENOUGH_MEMORY;
+			break;
+		}
+
+		// Add the trailing '\0', taking into account UNICODEness
+		//reinterpret_cast<TCHAR*>(pcbData)[ dwSize/sizeof(TCHAR) - 1 ] = _T('\0');
+		ZeroMemory( pcbData, (dwSize + sizeof(TCHAR)) );
+
+
+		// Query for the actual value
+		lResult = RegQueryValueEx( hKeyResult, lpValueName, 0, NULL, pcbData, &dwSize );
+		// Sanity Check
+		if( ERROR_SUCCESS != lResult ) 
+		{ 
+			break; 
+		}
+
+		// Paydirt...
+		szValue = reinterpret_cast<TCHAR*>(pcbData);
+	} while (FALSE);
+	
+	// Cleanup...
+    if( NULL != hKeyResult ) { RegCloseKey( hKeyResult ); }
+    if( NULL != pcbData ) { delete[] pcbData; }
+	
+    return lResult;
 }
--- ListCtrl/ListCtrlDemo/LicenseMgr.h
+++ ListCtrl/ListCtrlDemo/LicenseMgr.h
@@ -19,6 +19,9 @@ public:
 	BOOL IsRegistered();
 	BOOL GetLicenseInfo(CString& szName, CString& szCode);
 	BOOL Register(const CString& szName, const CString& szCode);
+
+	LONG WriteRegString(HKEY hKey, LPCTSTR lpSubKey, LPCTSTR lpValueName, LPCTSTR pszData);
+	LONG ReadRegString(HKEY hKey, LPCTSTR lpSubKey, LPCTSTR lpValueName, CString &szValue);
 };
 
 #endif // !defined(AFX_LICENSEMGR_H__1F6F14FE_2C39_4043_A2A4_8A80EEFC48B9__INCLUDED_)
