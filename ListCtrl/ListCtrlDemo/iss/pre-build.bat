1b516a8dc26fea998a07c1e47caed6049ba628c6
cconger77 <cconger77@2afb5f34-2eaf-11df-9ac8-ff20597b2c9b>
2011-07-28 04:35:10 +0000

    Update Iss Script

---
+++ ListCtrl/ListCtrlDemo/iss/pre-build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set SYSDLL_DIR=C:\WINDOWS\system32
+set DST_DIR=.\source
+
+if not exist "%DST_DIR%" mkdir "%DST_DIR%"
+
+copy /Y /B "%SYSDLL_DIR%\mfc42.dll" "%DST_DIR%"
+copy /Y /B "%SYSDLL_DIR%\msvcrt.dll" "%DST_DIR%"
+copy /Y /B "%SYSDLL_DIR%\msvcp60.dll" "%DST_DIR%"
+copy /Y /B "%GTB4WIN_HOME%\bin\gtb.dll" "%DST_DIR%"
+copy /Y /B "%LOG4CPLUS_HOME%\bin\log4cplus.dll" "%DST_DIR%"
+
+pause
